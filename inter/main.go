acbb582b2bd95090c92f32a65116aa3705d60b9c
Vivek <vivek.15@mountblue.tech>
2021-05-31 15:13:47 +0530

    Added data

---
+++ inter/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+type bot interface {
+	getGreeting() string
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(sb)
+	printGreeting(eb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
